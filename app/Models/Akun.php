696bf65491e663282e37cefa31ec92a26a0fdbcd
ikhsanananda <ikhsanananda@apps.ipb.ac.id>
2022-06-08 23:00:04 +0700

    api signup signin update delete getall

---
+++ app/Models/Akun.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Akun extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    public $table = 'users';
+    public $fillable = [
+        'name',
+        'email',
+        'password',
+        'telepon',
+        'rule'
+    ];
+
+    public $hidden = [];
+}
